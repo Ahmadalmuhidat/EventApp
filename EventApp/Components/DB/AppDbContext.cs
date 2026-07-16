538f5040f8df41280a7f341bb087bba1d3bc18f4
Ahmadalmuhidat <ahmad.almuhidat@gmail.com>
2026-07-16 19:57:53 +0300

    Add project files.

---
+++ EventApp/Components/DB/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EventApp;
+
+namespace EventApp;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(
+        DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+
+    public DbSet<EventModel> Events { get; set; }
+}
---
+++ EventApp/Components/Data/EventData.cs
@@ -0,0 +1,36 @@
+﻿using EventApp;
+
+namespace EventApp;
+
+public static class EventData
+{
+    public static List<EventModel> Events = new()
+    {
+        new EventModel
+        {
+            Id = 1,
+            Name = "Tech Conference 2026",
+            Date = new DateTime(2026,5,10),
+            Location = "Amman",
+            Description = "A conference about modern technology."
+        },
+
+        new EventModel
+        {
+            Id = 2,
+            Name = "Corporate Dinner",
+            Date = new DateTime(2026,6,15),
+            Location = "Dead Sea",
+            Description = "Annual company celebration."
+        },
+
+        new EventModel
+        {
+            Id = 3,
+            Name = "Startup Meetup",
+            Date = new DateTime(2026,7,20),
+            Location = "Irbid",
+            Description = "Networking event for startups."
+        }
+    };
+}
